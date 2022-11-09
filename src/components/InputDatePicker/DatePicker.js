24dd5038b5748d599089845bf03366f60432bef6
louzhonghua <2662554396@qq.com>
2022-11-09 17:15:41 +0800

    first commit

---
+++ src/components/InputDatePicker/DatePicker.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function DatePicker(props) {
+  return (
+    <div>DatePicker</div>
+  )
+}
+
+DatePicker.propTypes = {}
+
+export default DatePicker
---
+++ src/components/InputDatePicker/InputDatePicker.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import DatePicker from './DatePicker'
+
+function InputDatePicker(props) {
+  return (
+    <div>
+        <DatePicker />
+    </div>
+  )
+}
+
+InputDatePicker.propTypes = {}
+
+export default InputDatePicker
